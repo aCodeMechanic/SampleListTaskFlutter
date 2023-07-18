4935e9b58835bdd3a46bb9ccdc14c9f2cf63e384
aCodeMechanic <gurkaran.bambhrah@gmail.com>
2023-07-18 21:39:36 +0530

    :zap: Added valueMap as Map<String,int> in list.dart. Updated the ListItemWidget accordingly to update value on corresponding key in the map.

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # sample_list_task
 
-A new Flutter project.
+## Description
 
-## Getting Started
+- Load ListWidget and run the app.
+- Increment first list item by clicking it 3 times. Scroll to bottom and then scroll to top, the
+  count will get reset
 
-This project is a starting point for a Flutter application.
+## Functional/Product Requirement
 
-A few resources to get you started if this is your first Flutter project:
+- There should be 100 list items, each should be set at 0 initially and user can increment its
+  count.
+- The count incremented by user should be persisted when he scrolls the list.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+## Task:
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- [x] Solve the functional requirement.
+- [x] Review this file from Code Review perspective, list the issues and what changes should be made,
+  perform the changes to the file.
+- [x] Think of it as a intern checked in this code and as a senior has to code review and fix the code
+  from implementation perspective to make it correct and efficient.
+- [x] Send the fixed file back to the recruiter.
+
+[link to video](https://watch.screencastify.com/v/JNlGhNbIu48NkRmK9K3Y)
--- lib/list.dart
+++ lib/list.dart
@@ -1,42 +1,46 @@
 import 'package:flutter/material.dart';
 
+var valueMap = <String,int>{};
 class ListWidget extends StatelessWidget {
   const ListWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     var list = <Widget>[];
     for (var i = 0; i <= 100; i++) {
-      list.add(const ListItemWidget());
+      valueMap['$i'] = 0;
+      list.add(ListItemWidget(indexKey: '$i'));
     }
-    return ListView.builder(itemCount: list.length,itemBuilder: (context,index)=> list[index]);
+    return ListView.builder(
+        itemCount: list.length, itemBuilder: (context, index) => list[index]);
   }
 }
 
 class ListItemWidget extends StatefulWidget {
-  const ListItemWidget({super.key});
+  final String indexKey;
+
+  const ListItemWidget({super.key,required this.indexKey});
+
   @override
   State<ListItemWidget> createState() => _ListItemWidgetState();
 }
 
-
 class _ListItemWidgetState extends State<ListItemWidget> {
-  var count = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(4),
-        child: Row(
-          children: [
-            Text(count.toString()),
-            MaterialButton(
-              onPressed: () {
-                setState(() {
-                  count++;
-                });
-              },
-              child: const Text("+"),
-            )
-          ],
-        ));
+      padding: const EdgeInsets.all(4),
+      child: ListTile(
+        key: ValueKey<String>(widget.indexKey),
+        title: Text(valueMap[widget.indexKey].toString()),
+        trailing: MaterialButton(
+          onPressed: () {
+              valueMap[widget.indexKey] = valueMap[widget.indexKey]! + 1;
+          setState(() {});
+          },
+          child: const Text("+"),
+        ),
+      ),
+    );
   }
 }
